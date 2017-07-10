b0d1daa559fa115de6be383110bca65eb9903a82
LHS <LHS@DESKTOP-A48G9NV>
2017-07-10 19:44:28 +0900

    3-4 video version(계산기 기본 기능 구현 완료)

--- src/Calculator.java
+++ src/Calculator.java
@@ -11,36 +11,39 @@ public class Calculator {
 		int result = first;
 		int next;
 		String symbol;
-		
+
 		while (true) {
 			System.out.println("기호 : (종료: quit)");
 			symbol = scanner.next();
 			System.out.println(symbol);
 
-			if ("quit".equals(symbol)) {
-				// TODO 최종 실행 결과 값 출력
-				System.out.printf("계산 결과 : %d", result);
+			if (symbol.equals("quit")) {
+				System.out.printf("최종 결과 값 : %d", result);
 				break;
-			} 
-			
-			System.out.println("다음 값 입력: ");
-			next = scanner.nextInt();
-			System.out.println(next);
-			
-			// TODO 사칙연산 실행			
+			}
+
+			System.out.println("두 번째 입력 값: ");
+			int second = scanner.nextInt();
+			System.out.println(second);
+
+			// TODO 사칙연산 실행
 			if (symbol.equals("+")) {
-				result += next;
+				result = result + second;
+				System.out.println("덧셈 : " + result);
 			} else if (symbol.equals("-")) {
-				result -= next;
+				result = result - second;
+				System.out.println("뺄셈 : " + result);
 			} else if (symbol.equals("*")) {
-				result *= next;
+				result = result * second;
+				System.out.println("곱셈 : " + result);
 			} else if (symbol.equals("/")) {
-				result /= next;
+				result = result / second;
+				System.out.println("나눗셈 : " + result);
 			} else {
-				System.out.println("사칙연산에 해당하지 않는 값을 입력했습니다.");
+				System.out.println("사칙연산 기호가 아닙니다.");
 			}
 		}
-		
+
 		scanner.close();
 
 	}
