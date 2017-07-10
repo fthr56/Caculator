fa287a0c28dd77f05a6cce7c3ed98ddcd92dd96b
LHS <LHS@DESKTOP-A48G9NV>
2017-07-10 19:17:58 +0900

    3-3 video version

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,25 +8,24 @@ public class Calculator {
 		int first = scanner.nextInt();
 		System.out.println(first);
 
+		System.out.println("사칙연산 기호: ");
+		String symbol = scanner.next();
+		
 		System.out.println("두 번째 입력 값: ");
 		int second = scanner.nextInt();
 		System.out.println(second);
 
-		System.out.println("계산할 사칙연산을 입력: ");
-		String symbol = scanner.next();
-		
-		if("+".equals(symbol)) {
-				System.out.printf("계산 결과 : %d", first+second);
-		} else if ("-".equals(symbol)) {
-			System.out.printf("계산 결과 : %d", first-second);
-		} else if ("*".equals(symbol)) {
-			System.out.printf("계산 결과 : %d", first*second);
-		} else if ("/".equals(symbol)) {
-			System.out.printf("계산 결과 : %d", first/second);
+		if(symbol.equals("+")) {
+				System.out.printf("덧셈 : %d", first+second);
+		} else if (symbol.equals("-")) {
+			System.out.printf("뺄셈 : %d", first-second);
+		} else if (symbol.equals("*")) {
+			System.out.printf("곱셈 : %d", first*second);
+		} else if (symbol.equals("/")) {
+			System.out.printf("나눗셈 : %d", first/second);
 		} else {
 		  System.out.println("사칙연산에 해당하지 않는 값을 입력했습니다.");
 		}
-		
 		scanner.close();
 	}
 }
