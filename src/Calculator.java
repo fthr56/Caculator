38a83dcef0dee5f4a0c2a085a0dcd537fb27fd6c
LHS <LHS@DESKTOP-A48G9NV>
2017-07-10 19:32:26 +0900

    3-4 my version

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,24 +8,40 @@ public class Calculator {
 		int first = scanner.nextInt();
 		System.out.println(first);
 
-		System.out.println("사칙연산 기호: ");
-		String symbol = scanner.next();
+		int result = first;
+		int next;
+		String symbol;
 		
-		System.out.println("두 번째 입력 값: ");
-		int second = scanner.nextInt();
-		System.out.println(second);
+		while (true) {
+			System.out.println("기호 : (종료: quit)");
+			symbol = scanner.next();
+			System.out.println(symbol);
 
-		if(symbol.equals("+")) {
-				System.out.printf("덧셈 : %d", first+second);
-		} else if (symbol.equals("-")) {
-			System.out.printf("뺄셈 : %d", first-second);
-		} else if (symbol.equals("*")) {
-			System.out.printf("곱셈 : %d", first*second);
-		} else if (symbol.equals("/")) {
-			System.out.printf("나눗셈 : %d", first/second);
-		} else {
-		  System.out.println("사칙연산에 해당하지 않는 값을 입력했습니다.");
+			if ("quit".equals(symbol)) {
+				// TODO 최종 실행 결과 값 출력
+				System.out.printf("계산 결과 : %d", result);
+				break;
+			} 
+			
+			System.out.println("다음 값 입력: ");
+			next = scanner.nextInt();
+			System.out.println(next);
+			
+			// TODO 사칙연산 실행			
+			if (symbol.equals("+")) {
+				result += next;
+			} else if (symbol.equals("-")) {
+				result -= next;
+			} else if (symbol.equals("*")) {
+				result *= next;
+			} else if (symbol.equals("/")) {
+				result /= next;
+			} else {
+				System.out.println("사칙연산에 해당하지 않는 값을 입력했습니다.");
+			}
 		}
+		
 		scanner.close();
+
 	}
 }
